ed0fc4cd540e112b4ba3ff53eb41f127106ea2f5
zohaib166 <zohaib166@gmail.com>
2026-09-08 10:48:03 +0530

    from function to struct

--- distance.cpp
+++ distance.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-double find_distance(double x1, double x2, double y1, double y2)
+
+double find_dist(double x1, double y1, double x2, double y2)
 {
     double dist = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
     return dist;
 }
+
 int main()
 {
     double x1, y1, x2, y2, dist;
@@ -13,7 +15,8 @@ int main()
     cin >> x1 >> y1;
     cout << "Enter the second point (x2 and y2 separated by space)" << endl;
     cin >> x2 >> y2;
-    // call the function
+    // call function
+    double dist = find_dist(x1, y1, x2, y2);
     cout << "The distance is: " << dist << endl;
     cout << "Total Size: " << sizeof(x1) + sizeof(x2) + sizeof(y1) + sizeof(y2) + sizeof(dist) << endl;
     return 0;
---
+++ distance_struct.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+struct point
+{
+    double x, y;
+};
+
+int main()
+{
+    struct point p1;
+    return 0;
+}
